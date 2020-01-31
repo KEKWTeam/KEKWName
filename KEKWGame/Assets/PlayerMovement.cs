afc9f7936759d668bc1326ae7932e90ba740d362
Ethan <ethanavarro@gmail.com>
2020-01-31 18:18:52 +0100

    Añadido Salto del Jugador

---
+++ KEKWGame/Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    Rigidbody2D rb;
+    public float jumpforce = 1;
+    bool canjump = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && canjump) {
+            canjump = false;
+
+            rb.AddForce(Vector2.up * jumpforce);
+        }
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.gameObject.tag); //Debug del objeto con el que choca 
+        canjump = true;
+        if(col.gameObject.tag == "Ground"){ //Si el tag es Ground puede saltar 
+            canjump = true;
+        }
+    }
+
+    void Teclas() {
+    
+    
+    
+    }
+}
